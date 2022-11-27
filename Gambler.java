35a69851c4b2e2010ba448124c5b3f3b17691f81
Binoagustus <binoagustus@gmail.com>
2022-11-27 23:13:23 +0530

    decide win or lose after betting 1 dollar

--- Gambler.java
+++ Gambler.java
@@ -1,6 +1,10 @@
 package com.bl.gambling_simulator;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Gambler {
+	Scanner sc = new Scanner(System.in);
 	private short initialStake = 100;
 	private short bet = 1;
 
@@ -11,12 +15,28 @@ public class Gambler {
 	public short getBet() {
 		return bet;
 	}
-	
+
 	public void gamble(short stake, short bet) {
-		System.out.println("Initial Stake " + stake + " Bet " + bet);
-		if(bet == 1) {
-			System.out.println("Bet one dollar from Initial Stake");
+		System.out.println("Initial Stake " + stake);
+		System.out.println("Enter b to bet 1 dollar from Stake");
+		char pressKey = sc.next().charAt(0); 
+		if (bet == 1 && pressKey == 'b') {
+			boolean result = this.isWinOrLose();
+			if (result == true) {
+				System.out.println("win");
+			} else {
+				System.out.println("lose");
+			}
 		}
 	}
 
+	public boolean isWinOrLose() {
+		Random random = new Random();
+		int result = random.nextInt(2);
+		if (result == 1) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
